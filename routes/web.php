9a5ec77ffb05ef7b5f5a292c6c5ac26d0d6a6fa7
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-10-24 12:27:41 +0800

    設定Route回傳view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//原根路由
+//Route::get('/', function () { return view('welcome');});
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+Route::get('about',[AboutController::class,'index'])->name('about.index');
+
+Route::get('news',[NewsController::class,'index'])->name('news.index');
+
